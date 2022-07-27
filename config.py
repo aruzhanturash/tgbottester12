018c78faee65ed1495b8ebdafdbf69f9988a4965
aruzhanturash <aruzhanturash17@gmail.com>
2022-07-27 18:46:38 +0600

    first commit

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
-BOT_TOKEN = '5432438459:AAHgJ76DyGFWhWVojM1oJGfR31XaS0VmLzI'
+BOT_TOKEN = '5448354549:AAFDDjHniSNr1tmAo5vTyxF1X610s0tDk_Y'
 APP_URL = 'https://tgbottester1.herokuapp.com/' + BOT_TOKEN
 DB_URI = 'postgres://zdszdjjuvgcvel:59540a59601566f202d578681d50aef1089ed802222bef9dfa306c55582caf95@ec2-52-49-120-150.eu-west-1.compute.amazonaws.com:5432/d1c07bvmh278a1'
